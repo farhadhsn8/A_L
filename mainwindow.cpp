0a1d5b83f1eb71c018e3404728cd0050f0dd97e0
farhadhsn <farhadhsn8@gmail.com>
2019-07-27 19:22:57 +0430

    ui menu

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,17 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setCentralWidget(ui->menuwid);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+
+void MainWindow::on_okbtn_clicked()
+{
+
 }
